81b0ba52719f4e260821ee2baff6cee29736a811
agustinadiaz <agustina.diaz@ens-paris-saclay.fr>
2026-03-18 14:47:22 +0000

    multiple evaluation script and argument correction

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,16 +1,16 @@
 ARGS="
-    --model_name_or_path google/metricx-23-large-v2p0\
+    --model_name_or_path google/metricx-24-hybrid-large-v2p6\
     --dataset_name_or_path flores\
     --max_input_length 256\
     --batch_size 1\
     --data_dir /home/onyxia/work/llm-reasoning-mt-EXT/predictions\
-    --number_of_predictions 1012\
+    --number_of_predictions 300\
     --seed 122\
     --num_workers 8\
     --metric metricx\
     --languages Xhosa\
-    --strategies IOFT-gemma\
-    --names checkpoint-5000\
+    --strategies IOFT-qwen\
+    --names checkpoint-200 checkpoint-2200\
 "
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPO_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
